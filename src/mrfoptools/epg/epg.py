dbde602a6d2b74ac800a9a37d771ecf8002af87a
stebix <jannik.stebani@physik.uni-wuerzburg.de>
2025-01-20 04:31:43 +0100

    move signal functionality and implement static and dynamic grad shifting

--- src/mrfoptools/epg/epg.py
+++ src/mrfoptools/epg/epg.py
@@ -8,7 +8,7 @@ import numpy as np
 Array = jax.Array | np.ndarray
 
 # port the q_epg function from the signalmodel_epg.py file to JAX
-def q_epg(alpha: float, phi: float | None = None) -> Array:
+def q_epg(alpha: float, phi: float = jnp.pi / 2) -> Array:
     """Compute EPG excitation matrix for a given flip angle alpha and phase phi.
 
     Parameters
@@ -26,8 +26,6 @@ def q_epg(alpha: float, phi: float | None = None) -> Array:
     q : array
         EPG excitation matrix.
     """
-    phi = phi or jnp.pi / 2
-
     c1 = jnp.stack(
         [jnp.cos(alpha / 2) ** 2,
          jnp.exp(2 * 1j * phi) * jnp.sin(alpha / 2) ** 2,
@@ -228,14 +226,61 @@ def grad_shift(omega: Array, dk: int) -> Array:
     return omega_new
 
 
-def unit_grad_shift(omega: Array) -> Array:
-    """Apply an unit gradient shift to the EPG state."""
+def unit_grad_shift_allocating(omega: Array) -> Array:
+    """
+    Apply an unit gradient shift to the EPG state.
+
+    This is the allocating version that expands the state matrix on every
+    gradient application.
+    
+    Parameters
+    ----------
+
+    omega : Array
+        EPG state matrix of shape (3, n).
+
+    Returns
+    -------
+
+    omega_new : Array
+        EPG state matrix after applying the unit gradient shift.
+        New shape is (3, n+1).
+    """
     omega = jnp.hstack([omega, jnp.zeros((3, 1))])
     omega = omega.at[0, 1:].set(omega[0, :-1])
     omega = omega.at[1, :-1].set(omega[1, 1:])
     omega = omega.at[1, -1].set(0)
     omega = omega.at[0, 0].set(jnp.conjugate(omega[1, 0]))
     return omega
+
+
+def unit_grad_shift_static(omega: Array) -> Array:
+    """
+    Apply an unit gradient shift to the EPG state.
+
+    This is the static version that does not expand the state matrix on every
+    gradient application but expects a large matrix preallocated to be able to contain
+    all EPG states of the simulation.
+
+    Parameters
+    ----------
+
+    omega : Array
+        EPG state matrix of shape (3, N).
+
+    Returns
+    -------
+
+    omega : Array
+        EPG state matrix after applying the unit gradient shift.
+        Shape is (3, N).
+    """
+    omega = omega.at[0, 1:].set(omega[0, :-1])
+    omega = omega.at[1, -1].set(0)
+    omega = omega.at[1, :-1].set(omega[1, 1:])
+    omega = omega.at[0, 0].set(jnp.conjugate(omega[1, 0]))
+    return omega
+
 
 
 def inversion(inversion_efficiency: float) -> Array:
@@ -317,78 +362,3 @@ def dT2_preparation_dT2(T2: float, T2_preptime: float) -> Array:
             [0.0, 0.0, T2_preptime / T2**2 * jnp.exp(-T2_preptime / T2)]
         ]
     )
-
-
-
-class PreparationType(Enum):
-    NONE = 0
-    INVERSION = 1
-    T2_PREPARATION = 2
-
-
-def compute_signal(
-        T1: float,
-        T2: float,
-        M0: float,
-        fa: Array,
-        preparations: list[PreparationType],
-        n_blocks: int,
-        n_shots: int,
-        TR: Array,
-        phases: Array,
-        TI: Array,
-        T2_preptimes: Array,
-        TE: float,
-        inversion_efficiency: float = 1.0,
-        delta_B1: float = 1.0,
-) -> Array:
-    """
-    Compute the MR signal for a given set of parameters.
-
-    Expected static arguments: {n_blocks, n_shots,}
-    """
-    fa = jnp.deg2rad(fa)
-    phases = jnp.deg2rad(phases)
-
-    r_TE = r_epg(T1, T2, TE)
-    b_TE = b_epg(T1, TE)
-    inv_op = inversion(inversion_efficiency)
-    # Build initial state matrix.
-    omega = jnp.array([[0.0], [0.0], [M0]])
-    # Build array to store signal.
-    signal = jnp.zeros((len(fa)*len(preparations)), dtype=jnp.complex64)
-
-    for blk_idx in range(n_blocks):
-
-        preparation = preparations[blk_idx]
-
-        if preparation is PreparationType.INVERSION:
-            omega = r_epg(T1, T2, TI[blk_idx]) @ inv_op @ omega
-            omega = omega.at[2, 0].set(omega[2, 0] + M0 * b_epg(T1, TI[blk_idx]))
-        
-        elif preparation is PreparationType.T2_PREPARATION:
-            omega = preparation_T2_op(T2, T2_preptimes[blk_idx]) @ omega
-        
-        else:
-            # is no preparation
-            pass
-
-        for shot_idx in range(n_shots):
-
-            glob_idx = blk_idx * len(fa) + shot_idx
-
-            # Excitation matrix
-            q_n = q_epg(delta_B1 * fa[glob_idx], phases[glob_idx])
-
-            # Update state matrix (excitation and relaxation during TE)
-            omega = r_TE @ q_n @ omega
-            omega = omega.at[2, 0].set(omega[2, 0] + M0 * b_TE)
-
-            # Compute and store MR signal
-            signal = signal.at[glob_idx].set(omega[0, 0] * jnp.exp(1j * phases[glob_idx]))
-
-            # Update state matrix (relaxation during TR - TE, gradient dephasing)
-            omega = grad_shift(r_epg(T1, T2, TR[glob_idx] - TE) @ omega, dk=1)
-            omega = omega.at[2, 0].set(omega[2, 0] + M0 * b_epg(T1, TR[glob_idx] - TE))
-
-    return signal
