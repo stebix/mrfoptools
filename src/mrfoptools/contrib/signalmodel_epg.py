146eed880b06bdce2a944907cd7e299b5d8d09d9
stebix <jannik.stebani@physik.uni-wuerzburg.de>
2025-01-24 21:20:05 +0100

    add/export function on similar abstraction level to enable comparison

--- src/mrfoptools/contrib/signalmodel_epg.py
+++ src/mrfoptools/contrib/signalmodel_epg.py
@@ -577,3 +577,44 @@ def calculate_orth_epg(t1: List[float], t2: List[float], beats: int, shots: int,
     d = torch.norm(torch.eye(len(t1)) - s @ torch.t(torch.conj(s)))
 
     return d
+
+
+
+
+def simulate_fisp_part(t1: List[float], t2: List[float], beats: int, shots: int, fa: Union[np.ndarray, torch.tensor], tr: Union[np.ndarray, torch.tensor], ph: Union[np.ndarray, torch.tensor], prep: List[int], ti: List[int], t2te: List[int], te: float, inv_eff: float=1., delta_B1: float=1.):
+    """
+    Function to calculate the orthogonality cost function for a number of tissues for a given MRF sequence using Boch/isochromat summation. 
+
+    Args:
+        t1 (list of float): longitudinal relaxation times in ms
+        t2 (list of float): transversal relaxation times in ms 
+        beats (int): number of magnetization prepared blocks
+        shots (int): number of excitations per block
+        fa (array/tensor): flip angles in degrees
+        tr (array/tensor): repetition times in ms
+        ph (array/tensor): excitation phases in degrees
+        prep (list of int): types of magnetization preparations (0: no prep, 1: inversion, 2: t2 prep)
+        ti (list of int): inversion times in ms
+        t2te (list of int): t2 prep times in ms
+        te (float): echo time in ms
+
+    Keyword Args: 
+        inv_eff (float): inversion efficiency
+        delta_B1 (float): B1 correction factor
+
+    Returns:
+        (tensor): orthogonality cost function
+    """
+
+    # Number of different tissues
+    n_tissues = len(t1)
+
+    # Initialize signal matrix
+    s = torch.zeros((n_tissues, beats*shots), dtype=torch.cfloat)
+
+    # Calculate signals for all tissues
+    for ii in range(n_tissues):
+        # Set m0 to 1 as the signals will be normalized anyway
+        s[ii] = calculate_signal_epg(t1[ii], t2[ii], 1, beats, shots, fa, tr, ph, prep, ti, t2te, te, inv_eff, delta_B1)
+
+    return s
