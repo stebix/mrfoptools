56805cf44009dc22dc1684f93b84e71b3e93e2b0
stebix <jannik.stebani@physik.uni-wuerzburg.de>
2025-01-26 02:48:35 +0100

    tentative implement gradient descent via Adam

--- src/mrfoptools/optimization/optimization.py
+++ src/mrfoptools/optimization/optimization.py
@@ -3,16 +3,20 @@ Optimize the MRF sequence.
 
 @author: Jannik Stebani 2025
 """
+import functools
 import jax
 import jax.numpy as jnp
 import numpy as np
 import tqdm
+import optax
 
 
 from jax import Array
 from jax.typing import ArrayLike
 
-from mrfoptools.epg.signal import PreparationType
+from mrfoptools.epg.signal.signal import PreparationType, prepare_inversion_omega
+
+import mrfoptools.epg.core as epg
 
 import mrfoptools.optimization.costfuncs as costfuncs
 
@@ -62,6 +66,7 @@ def optimize(
     return (fa_history, loss_history)
 
 
+from mrfoptools.epg.signal.signal import compute_signal_optimized
 
 
 def optimize2(
@@ -73,38 +78,73 @@ def optimize2(
         intial_fa: Array,
         TR: Array,
         phases: Array,
-        preparation: PreparationType,
         TI: float,
         TE: float,
         inversion_efficiency: float = 1.0,
-        delta_B1: float = 1.0
 ) -> Array:
     """
     Optimize with simple gradient descent.
     """
     # Initialize the flip angles
     fa = intial_fa.copy()
-    # Initialize the cost function
-    function = jax.jit(costfuncs.legacy_orthogonality_criterion,
-                       static_argnames=('preparation', 'inversion_efficiency', 'delta_B1'))
+
+    MAX_STATES: int = 1000
+
+    lower = jnp.deg2rad(1.0)
+    upper = jnp.deg2rad(90.0)
+    
+
+    def forward(T1, T2, fa, M0, phases, TR, TE, TI, inversion_efficiency):
+        b_TE = epg.b_epg(T1=T1, dt=TE)
+        r_TE = epg.r_epg(T1=T1, T2=T2, dt=TE)
+        inv_op = epg.inversion(inversion_efficiency)
+        omega = prepare_inversion_omega(T1=T1, T2=T2,
+                                        M0=M0,
+                                        TI=TI,
+                                        inversion_operator=inv_op,
+                                        max_states=MAX_STATES)
+        return compute_signal_optimized(
+            omega, T1, T2, M0, fa, phases, TR, TE, b_TE, r_TE
+        )
+    
+    forward = functools.partial(forward,
+                                M0=M0, phases=phases, TR=TR, TE=TE,
+                                TI=TI, inversion_efficiency=inversion_efficiency)
+
+    forward = jax.vmap(forward, in_axes=(0, 0, None))
+
+    def step(T1, T2, fa):
+        signals = forward(T1, T2, fa)
+        ortho_cost = costfuncs.orthogonality_criterion(signals)
+
+        signal_cost = - jnp.sum(jnp.linalg.norm(signals, ord=2, axis=1))
+
+        mabs_cost = - jnp.mean(jnp.abs(signals))
+
+        smoothness_cost = jnp.linalg.norm(fa[1:] - fa[:-1], ord=2)
+        return 50 * mabs_cost +  smoothness_cost
     
     # Initialize the gradient
-    grad = jax.grad(function, argnums=3)
-    grad_and_loss = jax.value_and_grad(function, argnums=3)
+    value_and_grad = jax.jit(jax.value_and_grad(step, argnums=2))
 
     fa_history = []
     loss_history = []
 
     # Perform the optimization
 
+    optimizer = optax.adam(learning_rate=step_size)
+    optim_state = optimizer.init(fa)
+
     for iteration in tqdm.trange(max_iterations):
         fa_history.append(fa)
         # Compute the gradient
-        value, gradient = grad_and_loss(
-            T1, T2, M0, fa, TR, phases, preparation, TI, TE, inversion_efficiency, delta_B1
-        )
-        # Update the flip angles
-        fa = fa - step_size * gradient
+        value, gradient = value_and_grad(T1, T2, fa)
+
+        update, optim_state = optimizer.update(gradient, optim_state, fa)
+        fa = optax.apply_updates(fa, update)
+
+        fa = optax.projections.projection_box(fa, lower=lower, upper=upper)
+        
         loss_history.append(value)
 
     return (fa_history, loss_history)
