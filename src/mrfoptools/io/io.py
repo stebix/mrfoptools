2135e37f63930f5515400c451fce8243f3554cb0
stebix <jannik.stebani@physik.uni-wuerzburg.de>
2025-01-28 14:42:05 +0100

    add preliminary implementation for storage

--- src/mrfoptools/io/io.py
+++ src/mrfoptools/io/io.py
@@ -1,16 +1,19 @@
 import pathlib
+import dataclasses
+import numpy as np
+
+from collections.abc import Mapping
+from typing import Any
 
 import zarr
 
-from collections.abc import Mapping
-import dataclasses
 
 
 @dataclasses.dataclass
 class OptimizationBag:
-    settings: Mapping
-    results: Mapping
-    histories: Mapping 
+    settings: Mapping[str, Any]
+    results: Mapping[str, np.ndarray]
+    histories: Mapping[str, np.ndarray] 
 
 
 
@@ -21,4 +24,19 @@ def store_optimization_bag(
     """
     Stpre optimimzation settings and results in a zarr file.
     """
-    zarr_file = zarr.convenience.open()
+    zarr_file = zarr.convenience.open(path)
+    # Optimization configuration data
+    for k, v in bag.settings.items():
+        zarr_file.attrs[k] = v
+
+    results_group = zarr_file.create_group('results')
+
+    for k, v in bag.results.items():
+        results_group.create_dataset(name=k, data=v)
+
+    histories_group = zarr_file.create_group('histories')
+
+    for k, v in bag.histories.items():
+        histories_group.create_dataset(name=k, data=v)
+
+    return path
