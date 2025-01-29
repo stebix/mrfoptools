56b4b2252843832890f015f05099b5f5f3fbacfa
stebix <jannik.stebani@physik.uni-wuerzburg.de>
2025-01-29 03:32:33 +0100

    expand storage options

--- src/mrfoptools/io/io.py
+++ src/mrfoptools/io/io.py
@@ -2,7 +2,7 @@ import pathlib
 import dataclasses
 import numpy as np
 
-from collections.abc import Mapping
+from collections.abc import Mapping, Sequence
 from typing import Any
 
 import zarr
@@ -14,6 +14,27 @@ class OptimizationBag:
     settings: Mapping[str, Any]
     results: Mapping[str, np.ndarray]
     histories: Mapping[str, np.ndarray] 
+
+
+def _store_bag_to_group(
+        group: zarr.hierarchy.Group,
+        bag: OptimizationBag
+) -> None:
+    """
+    Store optimization bag in a zarr group.
+    """
+    for k, v in bag.settings.items():
+        group.attrs[k] = v
+
+    results_group = group.create_group('results')
+
+    for k, v in bag.results.items():
+        results_group.create_dataset(name=k, data=v)
+
+    histories_group = group.create_group('histories')
+
+    for k, v in bag.histories.items():
+        histories_group.create_dataset(name=k, data=v)
 
 
 
@@ -40,6 +61,45 @@ def store_optimization_bag(
         histories_group.create_dataset(name=k, data=v)
 
     return path
+
+
+def store_optimization_bags(
+        path: str | pathlib.Path,
+        bags: Sequence[OptimizationBag] | Mapping[str, OptimizationBag]
+) -> pathlib.Path:
+    """
+    Store multiple optimization bags in a single zarr file.
+    Typical use case: Runs with very tight coupling/relationship, e.g.
+    repeated runs over different random seeds.
+
+    Parameters
+    ----------
+
+    path : str | pathlib.Path
+        Path to the zarr file to be created.
+
+    bags : Sequence[OptimizationBag] | Mapping[str, OptimizationBag]
+        Sequence of optimization bags or a mapping of bags with keys.
+        If a mapping is provided, the keys are used as group names in the zarr file.
+        If a sequence is provided, the groups are named 'bag-1', 'bag-2', etc.
+
+    Returns
+    -------
+
+    pathlib.Path
+        Path to the created zarr file.
+    """
+    if not isinstance(bags, Mapping):
+        bags = {f'bag-{i}': bag for i, bag in enumerate(bags, start=1)}
+
+    zarr_file = zarr.convenience.open(path)
+    for name, bag in bags.items():
+        group = zarr_file.create_group(name)
+        _store_bag_to_group(group, bag)
+
+    return path
+
+    
 
 
 def load_optimization_bag(
