7beebaf9ab4e5840511a75dddd09c5cb16c3327f
stebix <jannik.stebani@physik.uni-wuerzburg.de>
2025-02-17 01:20:45 +0100

    add facilities for simulated annealing optimization

---
+++ src/mrfoptools/optimization/simulated_annealing/perturbations.py
@@ -0,0 +1,6 @@
+"""
+Functions to perturb parameterizations (i.e. control point collections)
+of canonical optimization variables (TR arrays/patterns, FA arrays/patterns).
+
+@Author: Jannik Stebani 2025
+"""
