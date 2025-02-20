076d1d32c3f51b497e0064d3d01cd273a95cf4cd
stebix <jannik.stebani@physik.uni-wuerzburg.de>
2025-02-20 13:59:29 +0100

    fix: use correct indices for fixed x and floating y

--- src/mrfoptools/optimization/simulated_annealing/calibration.py
+++ src/mrfoptools/optimization/simulated_annealing/calibration.py
@@ -4,11 +4,11 @@ Calibration tooling for simulated annealing optimization.
 @Author: Jannik Stebani 2025
 """
 import enum
-from collections.abc import Callable
+from collections.abc import Callable, Mapping
 
 import jax
 import jax.numpy as jnp
-import tqdm
+from tqdm.autonotebook import tqdm, trange
 
 from mrfoptools.parameterization.expansion import expand_fa, expand_tr, expand
 from mrfoptools.parameterization.parameterization import (initialize_parameterization,
@@ -28,40 +28,37 @@ class VariableType(enum.Enum):
 
 
 
-def compute_perturbation_costs(
-    N: int,
-    n_controlpoints: int,
-    scale: float,
-    bounds: tuple[float, float],
+def sample_perturbation_costs(
+    sample_count: int,
+    variable: VariableType,
+    relscale: float,
+    bounds_specification: Mapping[VariableType, tuple[float, float]],
     cost_func: Callable[[jax.Array, jax.Array], jax.Array],
     key: jax.Array,
-    variable: VariableType
+    n_controlpoints: int,
+    *,
+    NR: int = 1000,
+    pbar_kwargs: Mapping | None = None
 ) -> tuple[list[PerturbedControlPoints], jax.Array]:
     
-    NR: int = 1000
-
-    key, subkey = jax.random.split(key, num=2)
-
-    indices = jax.random.randint(
-        key=subkey, shape=N, minval=0, maxval=n_controlpoints
-    )
-
     # TODO: Check if necessary
     # Restrict parameterization to this hypercube
-
-    fa_bounds = (jnp.deg2rad(0), jnp.deg2rad(90))
-    tr_bounds = (5.0, 1000.0)
-    x_bounds = (0, 1000)
-
+    fa_bounds = bounds_specification[VariableType.FA_YCOORDS]
+    tr_bounds = bounds_specification[VariableType.TR_YCOORDS]
+    fa_x_bounds = bounds_specification[VariableType.FA_XCOORDS]
+    tr_x_bounds = bounds_specification[VariableType.TR_XCOORDS]
 
     # cache holds the (pre, post) pairs of perturbed
     # control points/parameterizations
     cache: list[PerturbedControlPoints] = []
     costs: list[jax.Array] = []
+
+    defaults = {'leave' : False}
+    pbar_kwargs = defaults | (pbar_kwargs or {})
     
-    for index in tqdm.tqdm(indices):
+    for i in tqdm(range(sample_count), **pbar_kwargs):
         
-        (key, fa_init_key, tr_init_key, perturb_key) = jax.random.split(key, num=4) 
+        (key, fa_init_key, tr_init_key, index_key, perturb_key) = jax.random.split(key, num=5) 
         
         # Generate FA part of state vector
         fa_con_param_kwargs = {
@@ -91,8 +88,11 @@ def compute_perturbation_costs(
         # and keep the repetition time control points constant
         # For the variable time, it's obviously the other way round.
         if variable is VariableType.FA_XCOORDS:
+            # x coordinates of the FA control points are fixed at edge values
+            # thus indices are in the restricted range [1, n_controlpoints-1]
+            index = jnp.random.randint(index_key, minval=1, maxval=n_controlpoints-1)
             fa_x_perturbed = perturb(
-                index=index, bounds=x_bounds, parameters=fa_con_pre.x, relscale=scale, key=perturb_key
+                index=index, bounds=fa_x_bounds, parameters=fa_con_pre.x, relscale=relscale, key=perturb_key
             )
             fa_con_perturbed = ControlPoints(fa_x_perturbed, fa_con_pre.y, edge_mode=fa_con_pre.edge_mode)
             cache.append(PerturbedControlPoints(fa_con_pre, fa_con_perturbed))
@@ -100,8 +100,11 @@ def compute_perturbation_costs(
             tr_post = tr_pre
             
         elif variable is VariableType.FA_YCOORDS:
+            # for y coordinates, we can perturb all control points
+            # thus the indices are in the full range [0, n_controlpoints]
+            index = jnp.random.randint(index_key, minval=0, maxval=n_controlpoints)
             fa_y_perturbed = perturb(
-                index=index, bounds=fa_bounds, parameters=fa_con_pre.y, relscale=scale, key=perturb_key
+                index=index, bounds=fa_bounds, parameters=fa_con_pre.y, relscale=relscale, key=perturb_key
             )
             fa_con_perturbed = ControlPoints(fa_con_pre.x, fa_y_perturbed, edge_mode=fa_con_pre.edge_mode)
             cache.append(PerturbedControlPoints(fa_con_pre, fa_con_perturbed))
@@ -109,8 +112,9 @@ def compute_perturbation_costs(
             tr_post = tr_pre
 
         elif variable is VariableType.TR_XCOORDS:
+            index = jnp.random.randint(index_key, minval=1, maxval=n_controlpoints-1)
             tr_x_perturbed = perturb(
-                index=index, bounds=x_bounds, parameters=tr_con_pre.x, relscale=scale, key=perturb_key
+                index=index, bounds=tr_x_bounds, parameters=tr_con_pre.x, relscale=relscale, key=perturb_key
             )
             tr_con_perturbed = ControlPoints(tr_x_perturbed, tr_con_pre.y, edge_mode=tr_con_pre.edge_mode)
             cache.append(PerturbedControlPoints(tr_con_pre, tr_con_perturbed))
@@ -118,8 +122,9 @@ def compute_perturbation_costs(
             fa_post = fa_pre
         
         elif variable is VariableType.TR_YCOORDS:
+            index = jnp.random.randint(index_key, minval=0, maxval=n_controlpoints)
             tr_y_perturbed = perturb(
-                index=index, bounds=tr_bounds, parameters=tr_con_pre.y, relscale=scale, key=perturb_key
+                index=index, bounds=tr_bounds, parameters=tr_con_pre.y, relscale=relscale, key=perturb_key
             )
             tr_con_perturbed = ControlPoints(tr_con_pre.x, tr_y_perturbed, edge_mode=tr_con_pre.edge_mode)
             cache.append(PerturbedControlPoints(tr_con_pre, tr_con_perturbed))
@@ -142,13 +147,14 @@ def compute_perturbation_costs(
 
 
 def compute_median_difference(
-    N: int,
-    n_controlpoints: int,
+    sample_count: int,
     variable: VariableType,
-    scale: float,
-    bounds: tuple[float, float],
+    relscale: float,
+    bounds_specification: Mapping[VariableType, tuple[float, float]],
     cost_func: Callable[[jax.Array, jax.Array], jax.Array],
-    key: jax.Array
+    key: jax.Array,
+    n_controlpoints: int,
+    pbar_kwargs: Mapping | None = None
 ) -> float:
     """
     Compute median cost difference for between
@@ -156,20 +162,26 @@ def compute_median_difference(
 
     Parameters
     ----------
-    N : int
+    sample_count : int
         Number of perturbations to compute.
-    n_controlpoints : int
-        Number of control points in the parameterization.
     variable : Variable
         Selector for the variable to perturb.
-    scale : float
-        Scale factors for the perturbation of the control points.
+    relscale : float
+        Relative scale factors for the perturbation of the control points.
+    bounds_specification : Mapping[Variable, tuple[float, float]]
+        Bounds specification for the control points.
     cost_func : Callable[[jax.Array, jax.Array], jax.Array]
         Cost function to evaluate the perturbed state vectors.
         Must accept two arguments: the full-siized FA and TR
         state vectors.
     key : jax.Array
         Random key for reproducible randomness.
+    n_controlpoints : int
+        Number of control points in the parameterization.
+    pbar_kwargs : Mapping, optional
+        Keyword arguments for the tqdm progress bar indicating
+        the progress of the perturbation cost sampling.
+        Default is None.
 
     Returns
     -------
@@ -177,8 +189,9 @@ def compute_median_difference(
         Median cost difference between initial and perturbed
         parameterizations.
     """
-    _, costs = compute_perturbation_costs(
-        N, n_controlpoints, scale, bounds, cost_func, key, variable
+    _, costs = sample_perturbation_costs(
+        sample_count, variable, relscale, bounds_specification, cost_func,
+        key, n_controlpoints, pbar_kwargs=pbar_kwargs
     )
     return float(jnp.median(costs))    
 
@@ -186,11 +199,36 @@ def compute_median_difference(
 
 def find_scale_factors(
     j_max: int,
-    fa_scale: tuple[float, float],
+    bounds_specification: Mapping[VariableType, tuple[float, float]],
+    cost_func: Callable[[jax.Array, jax.Array], jax.Array],
     key: jax.Array,
-    N: int = 500,
+    sample_count: int = 500,
 ) -> jax.Array:
     """
     Compute scale factors by least squares fitting.
     """
-    pass
+    w: float = 0.03
+    n_controlpoints = 9
+    median_differences: dict[VariableType, list[tuple[float, float]]] = {}
+
+    wrapped_variables = tqdm(VariableType)
+
+    for variable in wrapped_variables:
+        wrapped_variables.set_postfix_str(f'current: {variable}')
+        mdiffs: list[tuple[float, float]] = []
+        wrapped_js = trange(1, j_max + 1, leave=False)
+        for j in wrapped_js:
+            wrapped_js.set_postfix_str(f'current j={j}')
+            relscale = w * jnp.exp(-2 * j)
+            
+            key, subkey = jax.random.split(key)
+            
+            mdiff = compute_median_difference(
+                sample_count, variable, relscale, bounds_specification,
+                cost_func, subkey, n_controlpoints
+            )
+            mdiffs.append((relscale, mdiff))
+            
+        median_differences[variable] = mdiffs
+        
+    return median_differences
