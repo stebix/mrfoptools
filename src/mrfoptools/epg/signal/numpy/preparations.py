212583781e9ca18afec1c34fe0b98055daeca8d4
stebix <jannik.stebani@physik.uni-wuerzburg.de>
2025-03-14 13:43:16 +0100

    add numpy impl of preparations with jitting

--- src/mrfoptools/epg/signal/numpy/preparations.py
+++ src/mrfoptools/epg/signal/numpy/preparations.py
@@ -1,10 +1,15 @@
+import os
 import enum
 
 import numpy as np
+import numba as nb
 
 import mrfoptools.epg.core.numpy as npycore
 
 Array = np.ndarray
+
+NUMBA_JIT: bool = os.environ.get('NUMBA_JIT', '0') == '1'
+USE_FASTMATH: bool = os.environ.get('NUMBA_FASTMATH', '0') == '1'
 
 class PreparationType(enum.Enum):
     """Magnetization preparation type."""
@@ -13,6 +18,22 @@ class PreparationType(enum.Enum):
     T2_PREPARATION = 2
 
 
+preparation_T2_op = nb.jit(
+    npycore.preparation_T2_op,
+    nopython=NUMBA_JIT, fastmath=USE_FASTMATH
+)
+r_epg = nb.jit(
+    npycore.r_epg,
+    nopython=NUMBA_JIT, fastmath=USE_FASTMATH
+)
+b_epg = nb.jit(
+    npycore.b_epg,
+    nopython=NUMBA_JIT, fastmath=USE_FASTMATH
+)
+
+
+
+@nb.jit(nopython=NUMBA_JIT, error_model='numpy', fastmath=USE_FASTMATH)
 def prepare_equilibrium_omega(M0: float, max_states: int) -> Array:
     """
     Create large static EPG state matrix for equilibrium state.
@@ -25,6 +46,7 @@ def prepare_equilibrium_omega(M0: float, max_states: int) -> Array:
     return omega
 
 
+@nb.jit(nopython=NUMBA_JIT, error_model='numpy', fastmath=USE_FASTMATH)
 def prepare_inversion_omega(T1: float,
                             T2: float,
                             M0: float,
@@ -35,11 +57,12 @@ def prepare_inversion_omega(T1: float,
     """
     Generate large static initial EPG magnetization state matrix for inversion preparation.
     """
-    omega = npycore.r_epg(T1, T2, TI) @ inversion_operator @ prepare_equilibrium_omega(M0, max_states)
-    omega[2, 0] = omega[2, 0] + M0 * npycore.b_epg(T1, TI)
+    omega = r_epg(T1, T2, TI) @ inversion_operator @ prepare_equilibrium_omega(M0, max_states)
+    omega[2, 0] = omega[2, 0] + M0 * b_epg(T1, TI)
     return omega
 
 
+@nb.jit(nopython=NUMBA_JIT, error_model='numpy', fastmath=USE_FASTMATH)
 def prepare_T2_omega(T2: float,
                      T2_prep_time: float,
                      M0: float,
@@ -48,10 +71,11 @@ def prepare_T2_omega(T2: float,
     """
     Generate large static initial EPG magnetization state matrix for T2 preparation.
     """
-    omega = npycore.preparation_T2_op(T2, T2_prep_time) @ prepare_equilibrium_omega(M0, max_states)
+    omega = preparation_T2_op(T2, T2_prep_time) @ prepare_equilibrium_omega(M0, max_states)
     return omega
 
 
+@nb.jit(nopython=NUMBA_JIT, error_model='numpy', fastmath=USE_FASTMATH)
 def prepare_omega(preparation: PreparationType,
                   max_states: int,
                   T1: float,
@@ -101,3 +125,6 @@ def prepare_omega(preparation: PreparationType,
     else:
         msg = f'Invalid preparation type: {preparation}'
         raise ValueError(msg)
+
+
+#nb.jit_module(nopython=NUMBA_JIT, error_model='numpy')
