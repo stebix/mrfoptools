6d420ff10c7b999c43b2a8e272bc49644920f5aa
stebix <jannik.stebani@physik.uni-wuerzburg.de>
2025-03-17 23:41:25 +0100

    expand testing coverage

---
+++ tests/test_bag.py
@@ -0,0 +1,57 @@
+from pathlib import Path
+
+import numpy as np
+import zarr
+
+from mrfoptools.io.bag import store_datalike, store_metadatalike
+
+
+
+
+def test_store_datalike(tmp_path):
+    test_dir = tmp_path / 'test-1'
+    test_dir.mkdir()
+
+    NR = 100
+    NSTEPS = 10
+
+    array_path = test_dir / 'array.zarr'
+    test_group_name = 'test_group'
+    test_group_path = array_path / test_group_name
+
+    array = zarr.open(array_path, mode='w-')
+
+    test_group = array.create_group(test_group_name)
+    metadata_group = test_group.create_group('metadata')
+
+
+    data = {
+        'test_array_1' : np.zeros((NSTEPS, NR)),
+        'test_array_2' : np.full((NSTEPS, NR), fill_value=25),
+        'test_irregular_array' : {
+            'sampling_steps' : np.arange(33),
+            'sampling_values' : np.full((33, 15), fill_value=1337)
+        }
+    }
+
+    metadata = {
+        'test_metadata_1' : 'test',
+        'parameter' : 1000,
+        'learning_rate' : 0.01,
+        'in_depth_settings' : {
+            'n_iterations' : 1000,
+            'n_samples' : 100,
+            'bathtub_parameters' : {
+                'alpha' : 0.1,
+                'beta' : 0.3,
+                'gamma' : 0.7,
+                'delta' : 2.5
+            }
+        }
+    }
+
+    store_datalike(test_group, data)
+    store_metadatalike(metadata_group, metadata)
+
+    print(test_group)
+
