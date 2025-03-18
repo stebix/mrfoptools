8c628d2116ca780c957d75c555741b9c0e856de4
stebix <jannik.stebani@physik.uni-wuerzburg.de>
2025-03-18 23:40:34 +0100

    correctly implement tests

--- tests/test_bag.py
+++ tests/test_bag.py
@@ -3,9 +3,7 @@ from pathlib import Path
 import numpy as np
 import zarr
 
-from mrfoptools.io.bag import store_datalike, store_metadatalike
-
-
+from mrfoptools.io.bag import store_datalike, store_metadatalike, load_group, datamappings_are_equal
 
 
 def test_store_datalike(tmp_path):
@@ -17,14 +15,12 @@ def test_store_datalike(tmp_path):
 
     array_path = test_dir / 'array.zarr'
     test_group_name = 'test_group'
-    test_group_path = array_path / test_group_name
-
-    array = zarr.open(array_path, mode='w-')
-
-    test_group = array.create_group(test_group_name)
-    metadata_group = test_group.create_group('metadata')
-
-
+    metadata_group_name = 'metadata'
+    # set up zarr array structure on file system store
+    root = zarr.create_group(array_path, overwrite=False)
+    test_group = root.create_group(test_group_name, overwrite=False)
+    metadata_group = root.create_group(metadata_group_name, overwrite=False)
+    # set up mock data
     data = {
         'test_array_1' : np.zeros((NSTEPS, NR)),
         'test_array_2' : np.full((NSTEPS, NR), fill_value=25),
@@ -33,7 +29,6 @@ def test_store_datalike(tmp_path):
             'sampling_values' : np.full((33, 15), fill_value=1337)
         }
     }
-
     metadata = {
         'test_metadata_1' : 'test',
         'parameter' : 1000,
@@ -49,9 +44,11 @@ def test_store_datalike(tmp_path):
             }
         }
     }
-
     store_datalike(test_group, data)
     store_metadatalike(metadata_group, metadata)
-
-    print(test_group)
-
+    # load data back in
+    retrieved_data = load_group(test_group)
+    retrieved_metadata = dict(metadata_group.attrs)
+    # check equivalence
+    assert datamappings_are_equal(data, retrieved_data)
+    assert metadata == retrieved_metadata
