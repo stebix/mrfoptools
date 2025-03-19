a68534fa6ab11e544cac84be21e00d8a3d4f491c
stebix <jannik.stebani@physik.uni-wuerzburg.de>
2025-03-19 01:40:11 +0100

    finalize implementation

--- src/mrfoptools/optimization/diagnostics/plothelpers.py
+++ src/mrfoptools/optimization/diagnostics/plothelpers.py
@@ -3,14 +3,21 @@ PLotting utilities specialized for in-progress diagnostics and evaluation.
 
 @Author: Jannik Stebani 2025
 """
+import logging
 import attrs
 import matplotlib.pyplot as plt
 import numpy as np
 import jax
+
+from collections.abc import Mapping
 from matplotlib.axes import Axes
 from matplotlib.figure import Figure
 
+DEFAULT_LOGGER_NAME: str = '.'.join(('main', __name__))
+logger = logging.getLogger(DEFAULT_LOGGER_NAME)
+
 ArrayLike = np.ndarray | jax.Array
+
 
 class Plotter:
     """
@@ -51,65 +58,80 @@ class Plotter:
     def __init__(
         self,
         figsize: tuple[int, int] = (6, 4),
+        baseline_data: ArrayLike | None = None,
         title: str = '',
         xlabel: str = '',
         ylabel: str = '',
-        label: str = '',
         legend: bool = False,
         grid: bool = False,
         ylim: tuple[float, float] | None = None,
         xlim: tuple[float, float] | None = None,
         dpi: int = 100,
+        baseline_data_plot_kwargs: Mapping | None = None,
+        data_plot_kwargs: Mapping | None = None,
     ) -> None:
         self.figsize = figsize
+        self.baseline_data = baseline_data
         self.title = title
         self.xlabel = xlabel
         self.ylabel = ylabel
-        self.label = label
         self.legend = legend
         self.grid = grid
         self.ylim = ylim
         self.xlim = xlim
         self.dpi = dpi
+        self.baseline_data_plot_kwargs = baseline_data_plot_kwargs or {}
+        self.data_plot_kwargs = data_plot_kwargs or {}
+
 
     def generate(
         self,
-        y: ArrayLike,
+        data: ArrayLike,
     ) -> tuple[Figure, Axes]:
+        """
+        Generate a plot with the given data.
+        Data should be a 1D or 2D array.
+        """
         fig, ax = plt.subplots(figsize=self.figsize, dpi=self.dpi)
-        ax.plot(y, label=self.label)
-        ax.set_title(self.title)
-        ax.set_xlabel(self.xlabel)
-        ax.set_ylabel(self.ylabel)
-        ax.grid(self.grid)
-        if self.legend:
-            ax.legend()
-        if self.ylim:
-            ax.set_ylim(self.ylim)
-        if self.xlim:
-            ax.set_xlim(self.xlim)
+        if self.baseline_data is not None:
+            self.plot(ax, self.baseline_data, self.baseline_data_plot_kwargs)
+        self.plot(ax, data, self.data_plot_kwargs)
+        self.configure(
+            ax, self.title, self.xlabel, self.ylabel,
+            self.legend, self.grid, self.ylim, self.xlim)
         return (fig, ax)
 
 
-@attrs.define
-class PlotSettings:
-    title: str = ''
-    xlabel: str = ''
-    ylabel: str = ''
-    legend: bool = False
-    grid: bool = False
-    ylim: tuple[float, float] = None
+    @staticmethod
+    def plot(ax: Axes, data: ArrayLike, kwargs: Mapping) -> None:
+        if data.ndim == 1:
+            ax.plot(data, **kwargs)
+        elif data.ndim == 2:
+            for linedata in data:
+                ax.plot(linedata, **kwargs)
+        else:
+            msg = f'Can not plot data with more than 2 dimensions: got shape {data.shape}'
+            logger.error(msg)
 
 
-def configure(plot: tuple[Figure, Axes], settings: PlotSettings) -> None:
-    """Configure a matplotlib plot with the given settings.
-    """
-    fig, ax = plot
-    ax.set_title(settings.title)
-    ax.set_xlabel(settings.xlabel)
-    ax.set_ylabel(settings.ylabel)
-    ax.grid(settings.grid)
-    if settings.legend:
-        ax.legend()
-    if settings.ylim:
-        ax.set_ylim(settings.ylim)
+    @staticmethod
+    def configure(
+        ax: Axes,
+        title: str,
+        xlabel: str,
+        ylabel: str,
+        legend: bool,
+        grid: bool,
+        ylim: tuple[float, float] | None,
+        xlim: tuple[float, float] | None,
+    ) -> None:
+        ax.set_title(title)
+        ax.set_xlabel(xlabel)
+        ax.set_ylabel(ylabel)
+        ax.grid(grid)
+        if legend:
+            ax.legend()
+        if ylim:
+            ax.set_ylim(ylim)
+        if xlim:
+            ax.set_xlim(xlim)
