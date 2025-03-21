04f52e7b5def8e6046bbf8a594f9ebc7919c6fe8
stebix <jannik.stebani@physik.uni-wuerzburg.de>
2025-03-22 00:19:19 +0100

    refactor zarrinterface

--- src/mrfoptools/io/bag.py
+++ src/mrfoptools/io/bag.py
@@ -1,9 +1,8 @@
 import datetime
 import logging
-from collections.abc import Mapping, Sequence
-from typing import Any, TypeAlias, Union, NamedTuple
+from collections.abc import Mapping
+from typing import Any
 from pathlib import Path
-from numbers import Number
 
 import jax
 import numpy as np
@@ -11,21 +10,13 @@ import zarr
 
 from attrs import define, field
 
+import mrfoptools.io.zarrinterface as zarrinterface
+
 DEFAULT_LOGGER_NAME: str = '.'.join(('main', __name__))
 logger = logging.getLogger(DEFAULT_LOGGER_NAME)
 
 PathLike = str | Path
 Array = jax.Array | np.ndarray
-
-ArrayLike = np.ndarray | Sequence[Number]
-# DataMapping intended to reference nested dictionaries of numpy arrays
-# or sequences of numbers that can be written to zarr arrays directly.
-DataMapping: TypeAlias = Mapping[str, Union['DataMapping', ArrayLike]]
-
-# Jsonifiable intended to reference data types that can be serialized to JSON.
-# This is utilized for metadata mappings that are stored as attributes in zarr groups.
-Jsonifiable = str | int | float | bool | None | Mapping[str, 'Jsonifiable'] | Sequence['Jsonifiable']
-MetadataMapping: TypeAlias = Mapping[str, Union['MetadataMapping', Jsonifiable]]
 
 
 class Metadata:
@@ -53,110 +44,10 @@ class OptimizationBag:
 
 
 
-def datamappings_are_equal(
-    mapping_a: DataMapping,
-    mapping_b: DataMapping
-) -> bool:
-    """
-    Compare two data mappings for equality.
-    """
-    if mapping_a.keys() != mapping_b.keys():
-        return False
-
-    for key in mapping_a.keys():
-        value_a = mapping_a[key]
-        value_b = mapping_b[key]
-
-        if isinstance(value_a, Mapping):
-            if not datamappings_are_equal(value_a, value_b):
-                return False
-        else:
-            if not np.allclose(value_a, value_b):
-                return False
-
-    return True
-
-
-def store_datalike(
-    group: zarr.Group,
-    mapping: DataMapping
-) -> None:
-    """
-    Store a mapping of data-like objects to a Zarr store.
-    """
-    for key, value in mapping.items():
-        if isinstance(value, Mapping):
-            subgroup = group.create_group(key, overwrite=True)
-            store_datalike(subgroup, value)
-            continue
-
-        array = group.create_array(key, shape=value.shape, dtype=value.dtype, overwrite=True)
-        array[...] = value
-        logger.info(f'Successfully stored array with shape \'{array.shape}\' to {group}')
-    return None
-
-
-def store_metadatalike(
-    group: zarr.Group,
-    mapping: MetadataMapping
-) -> None:
-    """
-    Store a metadata mapping (i.e. mainly 'scalar' key - value pairs,
-    not substantial arrays) to a Zarr store.
-    """
-    group.attrs.update(mapping)
-    logger.info(f'Successfully stored metadata mapping to {group}')
-    return None
-
-
-class GroupKeys(NamedTuple):
-    array_keys: set[str]
-    subgroup_keys: set[str]
-
-def retrieve_keys(
-    group: zarr.Group
-) -> tuple[set[str], set[str]]:
-    """
-    Retrieve all keys of a `zarr.Group` object and sort into array keys and subgroup keys.
-
-    Parameters
-    ----------
-    group : zarr.Group
-        The group to retrieve keys from.
-
-    Returns
-    -------
-    GroupKeys
-        A named tuple with the keys sorted into array keys and
-    """
-    array_keys = set(group.array_keys())
-    subgroup_keys = set(group.keys()) - array_keys
-    return GroupKeys(array_keys=array_keys, subgroup_keys=subgroup_keys)
-
-
-ArrayMapping = Mapping[str, Union[np.ndarray, 'ArrayMapping']]  
-
-
-def load_group(
-    group: zarr.Group
-) -> DataMapping:
-    """
-    Eagerly load the content - arrays and subgroups - of a `zarr.Group` object
-    into a dictionary.
-    """
-    keys = retrieve_keys(group)
-    data = {
-        key : group[key][...]
-        for key in keys.array_keys
-    }
-    subdata = {key : load_group(group[key]) for key in keys.subgroup_keys}
-    return data | subdata
-
-
-
 def store_optimization_bag(
     bag: OptimizationBag,
     path: PathLike,
+    *,
     overwrite: bool = False
 ) -> None:
     """
@@ -173,22 +64,50 @@ def store_optimization_bag(
 
     # Store the protocol
     protocol_group = root.create_group('protocol', overwrite=overwrite)
-    store_metadatalike(protocol_group, bag.protocol)
+    zarrinterface.store(protocol_group, bag.protocol)
     # Store the hyperparameters
     hyperparameters_group = root.create_group('hyperparameters', overwrite=overwrite)
-    store_metadatalike(hyperparameters_group, bag.hyperparameters)
+    zarrinterface.store(hyperparameters_group, bag.hyperparameters)
     # Store the histories
     histories_group = root.create_group('histories', overwrite=overwrite)
-    store_datalike(histories_group, bag.histories)
+    zarrinterface.store(histories_group, bag.histories)
     # Store the initializations
     initializations_group = root.create_group('initializations', overwrite=overwrite)
-    store_datalike(initializations_group, bag.initializations)
+    zarrinterface.store(initializations_group, bag.initializations)
     # Store the results
     results_group = root.create_group('results', overwrite=overwrite)
-    store_datalike(results_group, bag.results)
+    zarrinterface.store(results_group, bag.results)
     # Store the metadata
     metadata_group = root.create_group('metadata', overwrite=overwrite)
-    store_metadatalike(metadata_group, bag.metadata)
+    zarrinterface.store(metadata_group, bag.metadata)
 
     logger.info(f'Successfully stored OptimizationBag to \'{path}\'')
     return None
+
+
+def load_optimization_bag(
+    path: PathLike
+) -> OptimizationBag:
+    """
+    Load an `OptimizationBag` object from a Zarr store.
+    """
+    path = Path(path) if not isinstance(path, Path) else path
+    store = zarr.storage.LocalStore(path)
+    root = zarr.group(store=store)
+
+    protocol = zarrinterface.load_group(root['protocol'])
+    hyperparameters = zarrinterface.load_group(root['hyperparameters'])
+    histories = zarrinterface.load_group(root['histories'])
+    initializations = zarrinterface.load_group(root['initializations'])
+    results = zarrinterface.load_group(root['results'])
+    metadata = zarrinterface.load_group(root['metadata'])
+
+    bag = OptimizationBag(
+        protocol=protocol,
+        hyperparameters=hyperparameters,
+        histories=histories,
+        initializations=initializations,
+        results=results,
+        metadata=metadata
+    )
+    return bag
