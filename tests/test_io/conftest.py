241f8737dd36d23f63b2af0b472eabf219223936
stebix <jannik.stebani@physik.uni-wuerzburg.de>
2025-03-22 01:39:51 +0100

    add more fixtures for typical OptimizationBag attributes

--- tests/test_io/conftest.py
+++ tests/test_io/conftest.py
@@ -19,6 +19,18 @@ def metadata() -> dict:
     return metadata
 
 
+@pytest.fixture
+def protocol() -> dict:
+    protocol = {
+        'sequence' : 'fisp',
+        'NR' : 1000,
+        'test' : 'nein-koenig-nein',
+        'T1' : [1, 2, 3, 4, 5, 6],
+        'T2' : [10, 20, 30, 40, 50, 60]
+    }
+    return protocol
+
+
 @pytest.fixture(params=(np, jnp))
 def data(request) -> dict:
     xnp = request.param
@@ -34,3 +46,39 @@ def data(request) -> dict:
         }
     }
     return data
+
+
+@pytest.fixture
+def initialization() -> dict:
+    initializations = {
+        'flipangles' : np.full(1000, fill_value=90.0),
+        'tr' : np.full(1000, fill_value=12.0),
+        'seed' : 1234
+    }
+    return initializations
+
+@pytest.fixture
+def results() -> dict:
+    return {}
+
+
+@pytest.fixture
+def hyperparameters() -> dict:
+    hyperparameter = {
+        'optimization' : 'gradient_descent',
+        'optimizer' : 'adam',
+        'test_metadata_1' : 'test',
+        'parameter' : 1000,
+        'learning_rate' : 0.01,
+        'in_depth_settings' : {
+            'n_iterations' : 1000,
+            'n_samples' : 100,
+            'bathtub_parameters' : {
+                'alpha' : 0.1,
+                'beta' : 0.3,
+                'gamma' : 0.7,
+                'delta' : 2.5
+            }
+        }
+    }
+    return hyperparameter
