151525b62999bb661c2b98c6fbd03f9435a59df0
stebix <jannik.stebani@physik.uni-wuerzburg.de>
2025-03-26 23:09:58 +0100

    refactor: rework project structure

--- src/mrfoptools/optimization/diagnostics/tensorboard.py
+++ src/mrfoptools/optimization/diagnostics/tensorboard.py
@@ -1,7 +1,5 @@
 from collections.abc import Mapping, Callable
-from numbers import Number
 
-import attrs
 import jax
 import jax.numpy as jnp
 from torch.utils.tensorboard import SummaryWriter
@@ -13,6 +11,7 @@ import mrfoptools.optimization.gradtools.gradtools as gradtools
 from mrfoptools.optimization.costgrad import CostContainer, GradientContainer
 
 from mrfoptools.optimization.diagnostics.plothelpers import Plotter
+from mrfoptools.optimization.diagnostics.references import RelativeGainEvaluator
 
 
 ArrayLike = np.ndarray | jax.Array
@@ -52,82 +51,6 @@ class _DiagnosticLogger:
         raise NotImplementedError
 
 
-
-@attrs.define
-class BaseCost:
-    """
-    Container for base cost values.
-
-    Parameters
-    ----------
-    costname : str
-        Name of the base cost function. This ID string is used
-        to match base costs with transient costs downstream.
-
-    refname : str
-        Name of the reference parameters that
-        were used to compute the base cost. This is typically
-        a certain initialization (e.g. Yun or Cao or constant pattern)
-
-    value : Number
-        Scalar value of the base cost computed with the reference
-        parameters.
-
-    range : diag.CostValueRange
-        Range of the cost value. Determines whether the cost value
-        is negative (always and unbounded) or positive.
-        Usage is for downstream relative gain computation.
-    """
-    costname: str
-    refname: str
-    value: Number
-    range: diag.CostValueRange
-
-
-class RelativeGainEvaluator:
-    """
-    Compute relative gains of cost functions with respect to base costs.
-    
-    Relative gains are computed by matching base costs from the initializer method
-    via their `costname` to supplied transient cost values in the
-    `transient_costs` argument of the `__call__` method.
-
-    Note: The `transient_costs` argument is expected to provide a cost value for 
-          each base cost in the initializer method. If a base cost is not found in
-          `transient_costs`, a `KeyError` is raised.
-    """
-    def __init__(
-        self,
-        *base_costs: BaseCost,
-        prefix: str = 'relative_gain'
-    ) -> None:
-        self.base_costs: list[BaseCost] = base_costs
-        self.prefix: str = prefix
-
-    def __call__(
-        self,
-        transient_costs: Mapping[str, CostContainer]
-    ) -> dict[str, float]:
-        
-        relative_gains: dict[str, float] = {}
-        for base_cost in self.base_costs:
-            logname = f'{self.prefix}/{base_cost.costname}-{base_cost.refname}'
-            cost_container = transient_costs[base_cost.costname]
-            if base_cost.range is diag.CostValueRange.POSITIVE:
-                relative_gain = base_cost.value / cost_container.cost
-            elif base_cost.range is diag.CostValueRange.NEGATIVE:
-                relative_gain = cost_container.cost / base_cost.value
-            else:
-                raise ValueError(f'Invalid cost value range: {base_cost.range}')
-            
-            relative_gains[logname] = relative_gain
-        return relative_gains
-            
-    def __str__(self):
-        return f'{self.__class__.__name__}(base_costs=\'{self.base_costs}\')'
-    
-    def __repr__(self):
-        return str(self)
 
 def rad2deg(fa: ArrayLike) -> ArrayLike:
     return jnp.rad2deg(fa)
