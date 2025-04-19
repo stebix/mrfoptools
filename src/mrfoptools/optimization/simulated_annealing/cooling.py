62dc4d590bd0f2088dfbd7d4ebbccea22df20a03
stebix <jannik.stebani@physik.uni-wuerzburg.de>
2025-04-19 02:59:12 +0200

    refactor: better function argument naming

--- src/mrfoptools/optimization/simulated_annealing/cooling.py
+++ src/mrfoptools/optimization/simulated_annealing/cooling.py
@@ -4,13 +4,13 @@ Cooling schedules for simulated annealing.
 import jax.numpy as jnp
 
 
-def linear_cooling(temp_start: float, r: float, iter: int) -> float:
-    return temp_start - r * iter
+def linear_cooling(temp_init: float, r: float, n_iter: int) -> float:
+    return temp_init - r * n_iter
 
 
-def exponential_cooling(temp_start: float, r: float, iter: int) -> float:
-    return temp_start * r ** iter
+def exponential_cooling(temp_init: float, r: float, n_iter: int) -> float:
+    return temp_init * r ** n_iter
 
 
-def logarithmic_cooling(temp_start: float, r: float, iter: int) -> float:
-    return temp_start / jnp.log1p(iter)
+def logarithmic_cooling(temp_init: float, r: float, n_iter: int) -> float:
+    return temp_init / jnp.log1p(n_iter)
